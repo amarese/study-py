f5355b7cbfd7f35ad14549e57407056f3985b645
Shawn <shawn.lee@snowcorp.com>
2017-11-06 18:40:21 +0900

    test

--- test-sift.py
+++ test-sift.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('data/model.jpg')
+img = cv2.imread('images/model.jpg')
 gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create()
