a42f71de7b93918331afc76e5725f8d76983dcd4
amarese <amarese@naver.com>
2018-05-30 14:38:38 +0900

    add debug log

--- test-haar.py
+++ test-haar.py
@@ -13,24 +13,29 @@ def face_detection(path, filename):
 
     # 세로사진만 취급한다.
     if height < width:
+        print("landscape width: ", width, ", height: ", height)
         return
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     # 한명이 나온 사진만.
-    if len(faces) != 1:
+    if not len(faces) == 1:
+        print("face count: ", len(faces))
         return
 
     x, y, w, h = faces[0]
 
-    # 얼굴이 전체의 1/10 이하여야 전신이 나올 수 있다.
-    if height / h > 10:
+    # 얼굴이 전체의 1/8 이하여야 전신이 나올 수 있다.(8등신 기준)
+    if height < 8 * h:
+        print("face ratio: ", h / height)
         return
 
     # 얼굴이 위쪽에 치우쳐 있어야 전신이 나올 수 있다.
-    if x + h > height / 3:
+    if (y + h) * 3 > height:
+        print("face position: ", (y + h) / height)
         return
 
+#    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     cv.imshow('img', img)
     cv.waitKey(0)
 
